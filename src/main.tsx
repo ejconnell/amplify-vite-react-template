c041ab3fae9dfdc2b521b74754a06b13b3605a7a
Ed C <ejconnell@gmail.com>
2025-04-27 22:53:15 +0900

    Change Cognito redirect to Cloudfront

--- src/main.tsx
+++ src/main.tsx
@@ -11,8 +11,8 @@ Amplify.configure(outputs);
 const cognitoAuthConfig = {
   authority: "https://cognito-idp.ap-southeast-2.amazonaws.com/ap-southeast-2_VQ0eXINVn",
   client_id: "6dmaip976mpqdf8tjs0q6n15qj",
-  /* redirect_uri: "https://d84l1y8p4kdic.cloudfront.net", */
-  redirect_uri: "http://localhost:5173/",
+  redirect_uri: "https://d84l1y8p4kdic.cloudfront.net",
+  /* redirect_uri: "http://localhost:5173/", */
   response_type: "code",
   scope: "phone openid email",
 };
