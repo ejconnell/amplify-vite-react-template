7f9f81b0703f7caec05ed790adf220b35ca52b85
Ed C <ejconnell@gmail.com>
2025-04-28 14:58:11 +0900

    Cognito localhost fix

--- src/main.tsx
+++ src/main.tsx
@@ -8,15 +8,24 @@ import { AuthProvider } from "react-oidc-context";
 
 Amplify.configure(outputs);
 
-const cognitoAuthConfig = {
+const remoteCognitoAuthConfig = {
   authority: "https://cognito-idp.ap-southeast-2.amazonaws.com/ap-southeast-2_VQ0eXINVn",
   client_id: "6dmaip976mpqdf8tjs0q6n15qj",
   redirect_uri: "https://main.d2c06f7nh1jalc.amplifyapp.com",
   /* redirect_uri: "https://d84l1y8p4kdic.cloudfront.net", */
-  /* redirect_uri: "http://localhost:5173/", */
   response_type: "code",
   scope: "phone openid email",
 };
+
+const localhostCognitoAuthConfig = {
+  authority: "https://cognito-idp.ap-southeast-2.amazonaws.com/ap-southeast-2_VQ0eXINVn",
+  client_id: "3h66igtcn7d3819or7a8ftfemc",
+  redirect_uri: "http://localhost:5173/",
+  response_type: "code",
+  scope: "email openid phone",
+};
+
+const cognitoAuthConfig = window.location.hostname === "localhost" ? localhostCognitoAuthConfig : remoteCognitoAuthConfig;
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
